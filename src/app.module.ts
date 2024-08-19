9528ebcf66c2b4db4445b0311bdb1a082223cf81
VitaliiGBunakov <v.g.bunakov@gmail.com>
2024-08-19 14:39:25 +0300

    initiated db connection to pg

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       cache: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_MYSQL_HOST || 'localhost',
+      port: parseInt(process.env.DB_MYSQL_PORT, 10 || 5432),
+      username: process.env.DB_MYSQL_USER || 'user',
+      password: process.env.DB_MYSQL_PASS || 'pass',
+      database: process.env.DB_MYSQL_DATABASE || 'test',
+      entities: [],
+      // synchronize: true,
     }),
   ],
   controllers: [AppController],
