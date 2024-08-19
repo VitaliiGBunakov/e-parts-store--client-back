4a4e9348df1aa8a01ac6d0da5136e8c4149aea33
VitaliiGBunakov <v.g.bunakov@gmail.com>
2024-08-19 11:16:12 +0300

    now linter ignores unused var near spread operator

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,13 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    '@stylistic/js/linebreak-style': ["error", "unix"]
+    '@stylistic/js/linebreak-style': ["error", "unix"],
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        "args": "all",
+        "ignoreRestSiblings": true
+      }
+    ]
   },
 };
